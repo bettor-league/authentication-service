5b4340a6df492ebfb9a71cdaa8addc577e648d79
Chabane Nadjim <c.nadjim@gmail.com>
2020-06-12 12:31:04 +0200

    Add Ci

--- src/test/java/com/bettorleague/authentication/AuthenticationServiceApplicationTests.java
+++ src/test/java/com/bettorleague/authentication/AuthenticationServiceApplicationTests.java
@@ -2,10 +2,9 @@ package com.bettorleague.authentication;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.springframework.boot.autoconfigure.mongo.embedded.EmbeddedMongoAutoConfiguration;
 import org.springframework.boot.test.autoconfigure.data.mongo.DataMongoTest;
-import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+
 
 
 @DataMongoTest
